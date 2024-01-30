81c1c2426e01e9124690ea6b813efb184bd9e6d4
Gayeon <gayeonkim@gimgayeon-ui-iMac.local>
2024-01-30 17:18:11 +0900

    gradle 세팅, spring boot

---
+++ src/main/java/practice/project/UknowTodoApplication.java
@@ -0,0 +1,12 @@
+package practice.project;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class UknowTodoApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(UknowTodoApplication.class, args);
+    }
+}
