9c6466190575e5f8d2a8dc4d9fe5a79f688a79aa
Gayeon <gayeonkim@gimgayeon-ui-iMac.local>
2024-02-15 14:47:53 +0900

    Jpa 설정

---
+++ src/main/java/practice/project/domain/entity/UserRepository.java
@@ -0,0 +1,8 @@
+package practice.project.domain.entity;
+
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+
+@Repository
+public interface UserRepository extends JpaRepository<User, Long> {
+}
