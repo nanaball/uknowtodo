d6f44d76be80cd6f8615a1a788c2b73d2d78dae1
Gayeon <gayeonkim@gimgayeon-ui-iMac.local>
2024-02-22 17:05:20 +0900

    설정 수정

--- src/main/java/practice/project/configuration/JpaConfig.java
+++ src/main/java/practice/project/configuration/JpaConfig.java
@@ -14,11 +14,12 @@ import org.springframework.orm.jpa.LocalContainerEntityManagerFactoryBean;
 import org.springframework.orm.jpa.vendor.HibernateJpaVendorAdapter;
 import org.springframework.transaction.PlatformTransactionManager;
 import org.springframework.transaction.annotation.EnableTransactionManagement;
+import practice.project.domain.entity.Entity;
 
 import javax.sql.DataSource;
 
 @Configuration
-@EnableJpaRepositories
+@EnableJpaRepositories(basePackageClasses = Entity.class)
 @EnableTransactionManagement
 @EnableJpaAuditing
 public class JpaConfig {
